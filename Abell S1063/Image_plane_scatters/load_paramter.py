e5d6f78c89ad97be51315fd37180857d810525e3
sang6199 <57351648+sang6199@users.noreply.github.com>
2023-01-16 17:30:55 +0900

    Add files via upload

---
+++ Abell S1063/Image_plane_scatters/load_paramter.py
@@ -0,0 +1,12 @@
+strong_lensing_file = 'AS1063_catalog.txt'
+interpol_size = 3000
+cluster_z = 0.348
+x1 = 0
+y1 = 0
+x2 = x1 + interpol_size
+y2 = y1 + interpol_size
+kapmap_size = 100
+margin = 20
+padding = int(2*margin + kapmap_size)
+core_num = 20
+free_param_redshift_source = None
