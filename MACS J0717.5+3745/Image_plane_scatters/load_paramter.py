48a43462064a1136984e27f12d7fe7a45921cffa
sang6199 <57351648+sang6199@users.noreply.github.com>
2023-01-16 17:19:56 +0900

    Add files via upload

---
+++ MACS J0717.5+3745/Image_plane_scatters/load_paramter.py
@@ -0,0 +1,12 @@
+strong_lensing_file = 'MACSJ0717_catalog.txt'
+interpol_size = 4000
+cluster_z = 0.545
+x1 = 0
+y1 = 0
+x2 = x1 + interpol_size
+y2 = y1 + interpol_size
+kapmap_size = 100
+margin = 20
+padding = int(2*margin + kapmap_size)
+core_num = 20
+free_param_redshift_source = 5, 8, 16, 17, 18, 20, 23, 25, 33, 34, 36, 37, 39, 45, 52, 56, 57, 65, 66, 67, 69, 76, 79, 80
